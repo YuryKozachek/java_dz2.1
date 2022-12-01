09470ec2acb2fff285c55ac7c1c9704cbbd4b96b
Yury Kozachek <show_@mail.ru>
2022-12-01 23:04:56 +0300

    first_commit_dz2.1

---
+++ src/javaDz2_1.java
@@ -0,0 +1,9 @@
+public class javaDz2_1 {
+    public static void main(String[] args) {
+        int priceTicket = 1000;
+        int bonusRub = 20;
+        int bonusMile;
+        bonusMile = priceTicket / bonusRub;
+        System.out.println(bonusMile);
+    }
+}
